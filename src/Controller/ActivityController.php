3ef5750644ee614ec9e80955de25fd97dca18180
istratan1998 <ionstratan74@gmail.com>
2019-05-22 16:13:34 +0300

    Added route and function for deleting an entry in Activity.

---
+++ src/Controller/ActivityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Activity;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+/**
+ * Activity controller.
+ * @Route("/api", name="api_")
+ */
+class ActivityController extends AbstractController
+{
+    /**
+     * Lists all Activities.
+     * @Rest\Get("/activities")
+     *
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function getActivitiesList(SerializerInterface $serializer): Response
+    {
+        $activities = $this->getDoctrine()->getRepository(Activity::class)->findAll();
+
+        /** @var SerializationContext $context */
+        $context = SerializationContext::create()->setGroups(array('ActivityList'));
+
+        $json = $serializer->serialize(
+            $activities,
+            'json',
+            $context
+        );
+
+        return new JsonResponse($json, 200, [], true);
+    }
+
+    /**
+     * Lists all Activities.
+     * @Rest\Get("/activities/{id}")
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function getActivityDetails(SerializerInterface $serializer, $id): Response
+    {
+        $activity = $this->getDoctrine()->getRepository(Activity::class)->find($id);
+
+        /** @var SerializationContext $context */
+        $context = SerializationContext::create()->setGroups(array('ActivityDetails'));
+
+        $json = $serializer->serialize(
+            $activity,
+            'json',
+            $context
+        );
+
+        return new JsonResponse($json, 200, [], true);
+    }
+
+    /**
+     * @Rest\Delete("/activities/delete/{id}")
+     * @param int $id
+     * @return Response
+     */
+
+    public function deleteActivity($id): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $activity =  $em->getRepository(Activity::class)->find($id);
+
+        if (!$activity) {
+            throw $this->createNotFoundException('No activity found for id '.$id);
+        }
+
+        $em->remove($activity);
+        $em->flush();
+
+        return new Response('Succesfully deleted!', Response::HTTP_OK);
+    }
+}
