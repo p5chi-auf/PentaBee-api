f9398f9e7b1db114ca01abe0aba567153a0e4c0e
Mihai Druta <mdruta@pentalog.com>
2019-07-08 17:20:04 +0300

    [BE] Adding user id in JWT payload https://trello.com/c/mB3WDYwH/70-be-adding-user-id-in-jwt-payload
    Added user id in JWT payload

--- src/JWTListener/JWTCreatedListener.php
+++ src/JWTListener/JWTCreatedListener.php
@@ -4,42 +4,18 @@ namespace App\JWTListener;
 
 use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class JWTCreatedListener
 {
-    /**
-     * @var RequestStack
-     */
-    private $requestStack;
-
-    /**
-     * JWTCreatedListener constructor.
-     * @param RequestStack $requestStack
-     */
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
-    }
-
     /**
      * @param JWTCreatedEvent $event
      */
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
-        $request = $this->requestStack->getCurrentRequest();
-
         $payload = $event->getData();
-        $payload['ip'] = $request->getClientIp();
         /** @var User $user */
         $user = $event->getUser();
         $payload['id'] = $user->getId();
-
         $event->setData($payload);
-
-        $header = $event->getHeader();
-        $header['cty'] = 'JWT';
-
-        $event->setHeader($header);
     }
 }
