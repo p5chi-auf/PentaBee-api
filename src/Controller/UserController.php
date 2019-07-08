dc38c3f5c7f51656778dbaeeebcc2f30f238e9b7
Mihai Druta <mdruta@pentalog.com>
2019-07-08 16:41:33 +0300

    [BE] Adding user id in JWT payload https://trello.com/c/mB3WDYwH/70-be-adding-user-id-in-jwt-payload
    Added user id in JWT payload

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,7 +17,6 @@ use JMS\Serializer\SerializerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
